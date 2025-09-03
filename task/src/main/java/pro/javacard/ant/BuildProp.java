3981d78a065ffded2d3f0a87ce6fa8e739d6523b
Michał Leszczyński <ml@icedev.pl>
2025-09-03 23:31:28 +0200

    add sanity check if the key is valid

--- task/src/main/java/pro/javacard/ant/BuildProp.java
+++ task/src/main/java/pro/javacard/ant/BuildProp.java
@@ -1,6 +1,12 @@
 package pro.javacard.ant;
 
+import org.apache.tools.ant.BuildException;
+
+import java.util.regex.Pattern;
+
 public class BuildProp {
+    static Pattern regex;
+
     String key;
     String value;
 
@@ -9,10 +15,18 @@ public class BuildProp {
     }
 
     public void setKey(String key) {
+        if (!regex.matcher(key).find()) {
+            throw new BuildException("The buildprop key contains illegal characters: " + key);
+        }
+
         this.key = key;
     }
 
     public void setValue(String value) {
         this.value = value;
+    }
+
+    static {
+        regex = Pattern.compile("^[a-zA-Z_][0-9a-zA-Z_]*$");
     }
 }
